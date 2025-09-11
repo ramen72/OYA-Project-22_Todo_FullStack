73c47070f65402965b12e47eefb856d041588fa0
Vercel <ramen@devsramen.com>
2025-09-11 17:39:24 +0600

    Update Login, forgot-password & reset-password  with ClassVideoPart-4 at 11-09-2025_1739

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,11 +95,112 @@ let verifyTokenController = async (req, res) => {
   }
 };
 
-let loginController = (req, res) => {
-  res.send("I am login AuthController");
+let loginController = async (req, res) => {
+  const { email, password } = req.body;
+  const userExists = await User.findOne({ email: email });
+  if (!userExists) {
+    return res.send({ error: `Invalid Credential ` });
+  } else if (!userExists.isVerified) {
+    return res.send({ error: `Please verify your email for login ` });
+  }
+
+  const isPasswordMatch = await bcryptjs.compare(password, userExists.password);
+  if (!isPasswordMatch) {
+    return res.send({ error: `Invalid Credential` });
+  }
+
+  const accessToken = generateAccessToken(userExists);
+  const refreshToken = generateRefreshToken(userExists);
+
+  userExists.refreshToken = refreshToken;
+
+  await userExists.save();
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    maxAge: 365 * 24 * 60 * 60 * 1000, // 1year validation
+  });
+
+  res.send({
+    message: "Login Successful",
+    accessToken: accessToken,
+    username: userExists.username,
+    email: userExists.email,
+  });
 };
+
+// Refresh Controller
+let refreshController = async (req, res) => {
+  const token = req.cookie.refreshToken;
+  if (!token) {
+    res.send(`No token found`);
+  }
+  const userExists = await User.findOne({ refreshToken: token });
+  if (!userExists) {
+    return res.send({ error: `Invalid Token` });
+  }
+
+  jwt.verify(token.process.env.REFRESH_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ error: `Invalid Token` });
+    }
+    const accessToken = generateAccessToken(userExists);
+    res.send({ accessToken });
+  });
+};
+
+// Forgot Password Controller
+let forgotPasswordController = async (req, res) => {
+  const { email } = req.body;
+  const userExists = await User.findOne({ email: email });
+  if (!userExists) {
+    return res.send({ error: `User Not Found` });
+  }
+  const resetToken = jwt.sign(
+    { id: userExists._id },
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: "15m",
+    }
+  );
+  const resetLink = `${process.env.CLIENT_URL}/reset-password/${resetToken}`;
+
+  await transporter.sendMail({
+    from: process.env.SENDING_MAIL_ID,
+    to: user.email,
+    subject: `Reset Password`,
+    html: `<h3>click to Reset Password : <a href=${verifyLink}>Reset Password</a></h3>`,
+  });
+
+  res.send({ message: `Please check your email for reset password.` });
+};
+
+// Reset Password Controller
+let resetPasswordController = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+    const userExists = await User.findById(decoded.id);
+    if (!userExists) {
+      res.send({ error: `Invalid token` });
+    }
+    userExists.password = await bcryptjs.hash(password, 10);
+    await userExists.save();
+    res.send({ message: `Password reset successfully done.` });
+  } catch (error) {
+    res.send({ error: `Token invalid or expired.` });
+  }
+};
+
 module.exports = {
   registrationController,
   loginController,
   verifyTokenController,
+  refreshController,
+  forgotPasswordController,
+  resetPasswordController,
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,10 +4,16 @@ const {
   registrationController,
   loginController,
   verifyTokenController,
+  refreshController,
+  forgotPasswordController,
+  resetPasswordController,
 } = require("../controllers/authController");
 
 router.post("/registration", registrationController);
 router.post("/verify/:token", verifyTokenController);
 router.post("/login", loginController);
+router.post("/refresh", refreshController);
+router.post("/forgot-password", forgotPasswordController);
+router.post("/reset-password:token", resetPasswordController);
 
 module.exports = router;
