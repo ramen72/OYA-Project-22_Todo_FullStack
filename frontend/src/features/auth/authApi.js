813f5e8dd242d2b03adc122bfd704442159bfcf2
Vercel <ramen@devsramen.com>
2025-09-24 16:23:58 +0600

    Update authApi.js

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -4,8 +4,5 @@ export const registration = (data) => api.post("/auth/registration", data);
 export const login = (data) => api.post("/auth/login", data);
 export const verifyEmail = (token) => api.get(`/auth/verify/${token}`);
 export const forgotPassword = (data) => api.post("/auth/forgot-password", data);
-export const resetPassword = (token, data) => {
-  console.log(token, data);
-};
-// export const resetPassword = (token, data) =>
-//   api.post(`/auth/reset-password/${token}`, data);
+export const resetPassword = (token, data) =>
+  api.post(`/auth/reset-password/${token}`, data);
