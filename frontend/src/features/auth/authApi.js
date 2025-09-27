72746f9ff2d36288b21f58327170b99b0a6d9445
ramen72 <ramen@devsramen.com>
2025-09-27 19:53:55 +0600

    Update authApi.js

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -1,6 +1,8 @@
 import api from "../../api";
 
-export const registration = (data) => api.post("/auth/registration", data);
+export const registration = (data) => {
+  api.post("/auth/registration", data);
+};
 export const login = (data) => api.post("/auth/login", data);
 export const verifyEmail = (token) => api.get(`/auth/verify/${token}`);
 export const forgotPassword = (data) => api.post("/auth/forgot-password", data);
