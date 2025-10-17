0f4cbc9bbbfa25a16797bfcd3bc0c55cbdd5f9c6
Ramen Biswas <ramen@devsramen.com>
2025-10-17 13:01:47 +0600

    Update CreateTodoPage.js at 17-10-2025_1301_v-20

--- frontend/src/pages/CreateTodoPage.jsx
+++ frontend/src/pages/CreateTodoPage.jsx
@@ -5,6 +5,8 @@ import api from "../api";
 
 const CreateTodoPage = () => {
   const [todo, setTodo] = useState("");
+  const [file, setFile] = useState({});
+  const [todoList, setTodoList] = useState([]);
   const [successMessage, setSuccessMessage] = useState("");
 
   const navigate = useNavigate();
@@ -12,7 +14,7 @@ const CreateTodoPage = () => {
     if (!localStorage.getItem("userInfo")) {
       navigate("/login");
     }
-  }, []);
+  }, [navigate]);
 
   useEffect(() => {
     // Call Data using api
@@ -27,25 +29,34 @@ const CreateTodoPage = () => {
           Authorization: `bearer ${
             JSON.parse(localStorage.getItem("userInfo")).accessToken
           }`,
+          // "Content-Type": "multipart/form-data",
         },
       })
       .then((data) => {
-        console.log(data);
+        setTodoList(data.data.todos);
+        console.log(data.data.todos);
       });
   }, []);
 
   const handleCreate = () => {
+    const formData = new FormData();
+    formData.append("text", todo);
+    formData.append("todoImage", file);
+
     axios
       .post(
         "http://localhost:3000/api/todo/createTodo",
-        {
-          text: todo,
-        },
+        formData,
+        // {
+        //   text: todo,
+        //   todoImage: file,
+        // },
         {
           headers: {
             Authorization: `bearer ${
               JSON.parse(localStorage.getItem("userInfo")).accessToken
             }`,
+            "Content-Type": "multipart/form-data",
           },
         }
       )
@@ -56,6 +67,11 @@ const CreateTodoPage = () => {
         }, 1000);
         console.log(data);
       });
+    setTodo("");
+    // setFile({});
+  };
+  const handleFile = (e) => {
+    setFile(e.target.files[0]);
   };
 
   return (
@@ -67,11 +83,20 @@ const CreateTodoPage = () => {
         </h1>
         <input
           type="text"
+          value={todo}
           name=""
           id=""
           placeholder="Write todo here."
           onChange={(e) => setTodo(e.target.value)}
           className={`w-[400px] border mr-5 p-2`}
+        />
+        <input
+          type="file"
+          name=""
+          id=""
+          onChange={handleFile}
+          // value={file}
+          className={`block border my-2 p-2`}
         />
         <button
           onClick={handleCreate}
@@ -80,6 +105,23 @@ const CreateTodoPage = () => {
           Create
         </button>
         {successMessage && <h4>{successMessage}</h4>}
+        <div>
+          {todoList.map((item, index) => (
+            <div className={`my-10 border roundeds mx-auto`}>
+              <img
+                src={item.mediaUrl}
+                alt=""
+                className={`mx-auto w-full h-auto`}
+              />
+              <h2
+                key={index}
+                className={`text-2xl text-center mt-2 capitalize`}
+              >
+                {item.text}
+              </h2>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
--- read.jsx
+++ read.jsx
@@ -1,1 +1,3 @@
-// Part-20_(00:30:00)
+// Part-20_(00:0:00) Completed
+
+// My$ecureP@ssw0rd
